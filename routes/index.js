48ce476413464b1983af7e878c053cdbb7e2416e
dinhtrung1605 <127390593+dinhtrungndt@users.noreply.github.com>
2023-08-07 18:27:35 +0700

    up load

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Nguyễn Đình Trưng - PK02294" });
+  res.render("index", { title: "Nguyễn Đình Trưng - Lake Ice" });
 });
 
 module.exports = router;
